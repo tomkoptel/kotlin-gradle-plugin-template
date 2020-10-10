e435d3fdf81f40768dd6f888edc79d38a7ab5c2d
Tom Koptel <tom.koptel@gmail.com>
2020-10-10 16:00:01 +0200

    Define listUnusedStrings

--- plugin-build/plugin/src/main/java/com/ncorti/kotlin/gradle/template/plugin/TemplatePlugin.kt
+++ plugin-build/plugin/src/main/java/com/ncorti/kotlin/gradle/template/plugin/TemplatePlugin.kt
@@ -1,21 +1,48 @@
 package com.ncorti.kotlin.gradle.template.plugin
 
+import com.android.build.gradle.AppPlugin
+import com.android.build.gradle.LibraryExtension
+import com.android.build.gradle.LibraryPlugin
+import com.android.build.gradle.internal.dsl.BaseAppModuleExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-
-const val EXTENSION_NAME = "templateExampleConfig"
-const val TASK_NAME = "templateExample"
+import org.gradle.api.tasks.Copy
 
 abstract class TemplatePlugin : Plugin<Project> {
     override fun apply(project: Project) {
-        // Add the 'template' extension object
-        val extension = project.extensions.create(EXTENSION_NAME, TemplateExtension::class.java, project)
+        project.plugins.withType(AppPlugin::class.java) {
+            val extension = project.extensions.getByName("android") as BaseAppModuleExtension
+            project.pluginManager.withPlugin("com.nimroddayan.lint-variant-baseline") {
+                extension.applicationVariants.all { variant ->
+                    createPluginTasks(project, variant.name)
+                }
+            }
+        }
+        project.plugins.withType(LibraryPlugin::class.java) {
+            val extension = project.extensions.getByName("android") as LibraryExtension
+            project.pluginManager.withPlugin("com.nimroddayan.lint-variant-baseline") {
+                extension.libraryVariants.all { variant ->
+                    createPluginTasks(project, variant.name)
+                }
+            }
+        }
+    }
 
-        // Add a task that uses configuration from the extension object
-        project.tasks.register(TASK_NAME, TemplateExampleTask::class.java) {
-            it.tag.set(extension.tag)
-            it.message.set(extension.message)
-            it.outputFile.set(extension.outputFile)
+    private fun createPluginTasks(
+        project: Project,
+        variantName: String
+    ) {
+        val variantNameCaps = variantName.capitalize()
+        val lintBaseline = "generateLintBaseline$variantNameCaps"
+
+        project.tasks.named(lintBaseline, Copy::class.java) {
+            val lintBaselineTask = this
+
+            val listUnusedTask = project.tasks.register("listUnusedStrings$variantNameCaps") { task ->
+                task.dependsOn(lintBaselineTask)
+                task.description = "Allows to list all unused strings for $variantNameCaps"
+                task.group = "Pre Lint"
+            }
         }
     }
 }
